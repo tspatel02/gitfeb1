32d0fe825b7caf5cd8d41e030c27a10c38834d1c
tspatel02 <tspatel02@yahoo.com>
2020-02-01 17:12:20 -0600

    initial copy

---
+++ feb1/src/main/java/com/itexps/marketing/Car.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.marketing;
+
+/**
+ *
+ * @author Owner
+ */
+public class Car {
+    String vin;
+    String color;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+}
---
+++ feb1/src/test/java/test/com/itexps/marketing/CarTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com.itexps.marketing;
+
+import com.itexps.marketing.Car;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class CarTest {
+    Car car;
+    
+    public CarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        car = new Car();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testCar() {
+     
+         car.setVin("aaa");
+         car.setColor("red");
+         
+         assertEquals("red", car.getColor());
+         assertEquals("aaa", car.getVin());
+         
+     }
+}
